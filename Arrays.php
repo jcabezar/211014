cafab598fd09dd1f9a83942bd982051177592a96
jcabezar <jcabeza76@hotmail.com.com>
2021-10-14 09:58:04 +0200

    commit ordenador

--- Arrays.php
+++ Arrays.php
@@ -1,1 +1,16 @@
 <?php
+$nombres=array('Ana', 'Belen', 'Carlos');
+
+echo $nombres[2];
+
+$nombres [2]='Diana';
+
+echo $nombres[2];
+
+print_r($nombres);
+
+$nombres[]='Ernesto';
+
+$direcciones['0000']='Ana';
+
+define('MAX', 20);
